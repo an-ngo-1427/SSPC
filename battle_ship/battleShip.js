ad98f13f83f2b9b72163c364b3076aed68101f39
An Ngo <anng0149@gmail.com>
2024-07-01 12:55:40 -0700

    drag and drop

--- battle_ship/battleShip.js
+++ battle_ship/battleShip.js
@@ -1,3 +1,22 @@
+// ships container
+const shipCont = document.createElement('div');
+shipCont.className = 'ship-container';
+function dragstartHandler(ev) {
+    // Add the target element's id to the data transfer object
+    ev.dataTransfer.setData("text/plain", ev.target.className);
+    ev.dataTransfer.effectAllowed = "move";
+  }
+function dragoverHandler(ev) {
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "move";
+  }
+  function dropHandler(ev) {
+    ev.preventDefault();
+    // Get the id of the target and add the moved element to the target's DOM
+    const data = ev.dataTransfer.getData("text/plain");
+    ev.target.className = data;
+  }
+
 // player board
 const self_field = document.createElement('div');
 self_field.className = 'self-field';
@@ -9,7 +28,15 @@ drawBoard(op_field,8,8)
 
 
 const body = document.querySelector('body');
-body.append(self_field,op_field);
+body.append(shipCont,self_field,op_field);
+
+// adding drag and drop elements
+const smallShip = document.createElement('div');
+smallShip.draggable = 'true';
+smallShip.className = 'blue box';
+smallShip.ondragstart = dragstartHandler;
+shipCont.append(smallShip);
+
 
 
 function drawBoard(element,numRows,numCols){
@@ -38,9 +65,11 @@ function drawBoard(element,numRows,numCols){
                 newBox.innerText = String.fromCharCode(64+i)
                 newBox.style.border='none'
             }
-            newBox.addEventListener('click',(e)=>{
-                e.target.style.background = 'blue'
-            })
+            // newBox.addEventListener('click',(e)=>{
+            //     e.target.style.background = 'blue'
+            // })
+            newBox.ondrop = dropHandler;
+            newBox.ondragover = dragoverHandler;
             row.appendChild(newBox)
         }
         element.appendChild(row)
